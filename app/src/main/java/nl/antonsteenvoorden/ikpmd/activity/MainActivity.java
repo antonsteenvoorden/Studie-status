92ffcb562fa4f8e70da6f7a06d946764e38bb249
Daan Rosbergen <d.rosbergen@gmail.com>
2015-12-30 15:02:32 +0100

    Fix imports

--- app/src/main/java/nl/antonsteenvoorden/ikpmd/activity/MainActivity.java
+++ app/src/main/java/nl/antonsteenvoorden/ikpmd/activity/MainActivity.java
@@ -1,43 +1,28 @@
 package nl.antonsteenvoorden.ikpmd.activity;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.content.Intent;
-import android.os.Bundle;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import nl.antonsteenvoorden.ikpmd.App;
+import butterknife.OnClick;
 import nl.antonsteenvoorden.ikpmd.R;
-import nl.antonsteenvoorden.ikpmd.fragment.StandVanZaken;
 import nl.antonsteenvoorden.ikpmd.orm.Module;
-import nl.antonsteenvoorden.ikpmd.fragment.StandVanZaken;
 
 public class MainActivity extends AppCompatActivity {
 
