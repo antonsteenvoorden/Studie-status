1d54497f70f2da5668e3b0d2465708677475205a
Daan Rosbergen <d.rosbergen@gmail.com>
2015-12-31 20:49:02 +0100

    Revert merge commits

--- app/src/main/java/nl/antonsteenvoorden/ikpmd/activity/MainActivity.java
+++ app/src/main/java/nl/antonsteenvoorden/ikpmd/activity/MainActivity.java
@@ -2,21 +2,39 @@ package nl.antonsteenvoorden.ikpmd.activity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import nl.antonsteenvoorden.ikpmd.App;
 import nl.antonsteenvoorden.ikpmd.R;
 import nl.antonsteenvoorden.ikpmd.fragment.StandVanZaken;
-import nl.antonsteenvoorden.ikpmd.fragment.VakkenFragment;
+import nl.antonsteenvoorden.ikpmd.orm.Module;
+import nl.antonsteenvoorden.ikpmd.fragment.StandVanZaken;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences settings;
     @Bind(R.id.container) ViewPager mViewPager;
     @Bind(R.id.tabs) TabLayout tabLayout;
+    @Bind(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
         settings = getSharedPreferences(SplashScreen.PREFS_NAME, 0);
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        settings = getSharedPreferences(SplashScreen.PREFS_NAME, 0);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -53,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
         tabLayout.setupWithViewPager(mViewPager);
 
-        // Retrieve modules
-        ((App) getApplication()).getModuleService().findAll();
+        Module module = Module.load(Module.class, 1);
+        Log.d("ORM", module.toString());
     }
 
 
@@ -80,7 +103,41 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        @Bind(R.id.section_label) TextView textView;
 
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ButterKnife.bind(this, rootView);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -96,14 +153,8 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            switch(position){
-                case 0:
-                    return StandVanZaken.newInstance();
-
-                case 1:
-                    return VakkenFragment.newInstance();
-            }
-            return null;
+            return PlaceholderFragment.newInstance(position + 1);
+//            return StandVanZaken.newInstance("Test");
         }
 
         @Override
--- app/src/main/java/nl/antonsteenvoorden/ikpmd/fragment/StandVanZaken.java
+++ app/src/main/java/nl/antonsteenvoorden/ikpmd/fragment/StandVanZaken.java
@@ -1,115 +1,110 @@
 package nl.antonsteenvoorden.ikpmd.fragment;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-
-import java.util.ArrayList;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 import nl.antonsteenvoorden.ikpmd.R;
 
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link StandVanZaken.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link StandVanZaken#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class StandVanZaken extends Fragment {
-    @Bind(R.id.stand_van_zaken_label)
-    TextView textView;
-    private PieChart mChart;
-    public static final int MAX_ECTS = 60;
-    public static int currentEcts = 0;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
 
     public StandVanZaken() {
+        // Required empty public constructor
     }
 
     /**
-     * Returns a new instance of this fragment for the given section
-     * number.
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment StandVanZaken.
      */
-    public static StandVanZaken newInstance() {
+    // TODO: Rename and change types and number of parameters
+    public static StandVanZaken newInstance(String param1) {
         StandVanZaken fragment = new StandVanZaken();
         Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+         }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_stand_van_zaken, container, false);
-        ButterKnife.bind(this, rootView);
-        textView.setText("Stand van zaken");
-
-        mChart = (PieChart) rootView.findViewById(R.id.chart);
-        mChart.setDescription("");
-        mChart.setTouchEnabled(false);
-        mChart.setDrawSliceText(true);
-        mChart.getLegend().setEnabled(false);
-        mChart.setTransparentCircleColor(Color.rgb(130, 130, 130));
-        // mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
-        mChart.spin(500, 0, -360f, Easing.EasingOption.EaseInOutQuad);
-
-        setData(0);
-
-        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (currentEcts < MAX_ECTS) {
-                    setData(currentEcts += 2);
-                } else {
-                    setData(currentEcts = 0);
-                }
-            }
-        });
-
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("HOI");
         return rootView;
     }
 
-    private void setData(int aantal) {
-        currentEcts = aantal;
-        ArrayList<Entry> yValues = new ArrayList<>();
-        ArrayList<String> xValues = new ArrayList<>();
-
-        yValues.add(new Entry(aantal, 0));
-        xValues.add("Behaalde ECTS");
-
-        yValues.add(new Entry(60 - currentEcts, 1));
-        xValues.add("Resterende ECTS");
-
-        //  http://www.materialui.co/colors
-        ArrayList<Integer> colors = new ArrayList<>();
-        if (currentEcts <10) {
-            colors.add(Color.rgb(244,81,30));
-        } else if (currentEcts < 40){
-            colors.add(Color.rgb(235,0,0));
-        } else if  (currentEcts < 50) {
-            colors.add(Color.rgb(253,216,53));
-        } else {
-            colors.add(Color.rgb(67,160,71));
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
         }
-        colors.add(Color.rgb(255,0,0));
-
-        PieDataSet dataSet = new PieDataSet(yValues, "ECTS");
-        dataSet.setColors(colors);
-
-        PieData data = new PieData(xValues, dataSet);
-        mChart.setData(data); // bind dataset aan chart.
-        mChart.invalidate();  // Aanroepen van een redraw
-        Log.d("aantal =", ""+currentEcts);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
 }
