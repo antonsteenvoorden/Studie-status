78a688fcbadd6f5976d2536b363ca7e9f7807ca0
Anton Steenvoorden <Anton Steenvoorden>
2016-01-03 19:52:17 +0100

    Trying to get the selected items' properties to go into detailview

--- app/src/main/java/nl/antonsteenvoorden/ikpmd/fragment/VakkenFragment.java
+++ app/src/main/java/nl/antonsteenvoorden/ikpmd/fragment/VakkenFragment.java
@@ -11,12 +11,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -63,9 +66,6 @@ public class VakkenFragment extends Fragment {
         return rootView;
     }
 
-    void test() {
-
-    }
 
 
     @Override
@@ -79,6 +79,14 @@ public class VakkenFragment extends Fragment {
         lcAdapter = new VakkenCursorAdapter(context, content, 0);
 
         listViewItems.setAdapter(lcAdapter);
+
+        listViewItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("ITEM CLICKED", String.valueOf(parent.getItemAtPosition(position)));
+
+            }
+        });
 
 
     }
